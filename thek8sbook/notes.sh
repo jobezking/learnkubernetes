b94c9625fed5d43ecd4bb049107b630dfdf4d139
Job King <jobezking@gmail.com>
2026-07-11 20:43:22 -0400

    update

---
+++ thek8sbook/notes.sh
@@ -0,0 +1,7 @@
+kubectl get nodes
+kubectl config view [--flatten | --minify | --output='json' | --output='yaml']
+kubectl config get-contexts
+kubectl get pods
+kubectl explain pods --recursive | more
+kubectl explain pods.spec.containers
+kubectl explain pods.spec.restartPolicy
