5d00db36894c699a74098452f4a7e43ffaba4734
Job King <jobezking@gmail.com>
2026-07-13 21:45:56 -0400

    push

--- thek8sbook/notes.sh
+++ thek8sbook/notes.sh
@@ -30,4 +30,8 @@ kubectl get pods --namespace kube-system
 kubectl create ns hydra  # kubectl create namespace hydra 
 kubectl apply -f shield-namespace.yml
 kubectl get ns
+kubectl config set-context --current --namespace hydra
+kubectl get pods -n hydra
+kubectl get svc -n hydra
 kubectl delete ns hydra
+kubectl config set-context --current --namespace default
