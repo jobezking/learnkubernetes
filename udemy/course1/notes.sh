a4be96959ab19ecf51bddfa93360dce094dc6ea6
Job King <jobezking@gmail.com>
2026-08-18 15:28:09 -0400

    push

---
+++ udemy/course1/notes.sh
@@ -0,0 +1,10 @@
+Components
+API server: acts as front end for the Kubernetes control plane. It exposes the Kubernetes API and is responsible for handling REST requests, validating them, and updating the corresponding objects in etcd.
+etcd service: distributed reliable key-value store that stores all cluster data. It is used by the API server to persist the state of the cluster.
+Kubelet service: an agent that runs on each node in the cluster. It ensures that containers are running in a Pod and reports back to the API server.
+container runtime: underlying software that is responsible for running containers. It can be Docker, containerd, or any other compatible runtime.
+controller manager: a component that runs controller processes. Each controller is a separate process that watches the state of the cluster and makes changes to move the current state towards the desired state.
+scheduler: distributing work and containers across nodes
+
+master node: where the control plane components run. It is responsible for managing the cluster and making global decisions about the cluster (e.g., scheduling). The master node runs at least: API server, etcd, controller manager, and scheduler.
+worker nodes: where (Docker) containers run. Each worker node runs at least: kubelet, container runtime, and kube-proxy.
