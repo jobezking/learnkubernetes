c78d1edf38c68141374661147413e82c27e2ec93
Job King <jobezking@gmail.com>
2026-08-18 23:50:34 -0400

    push

--- udemy/course1/notes.sh
+++ udemy/course1/notes.sh
@@ -18,3 +18,4 @@ kubernetes.io/docs/tasks/tools
 3. https://minikube.sigs.k8s.io/docs/start/?arch=%2Flinux%2Fx86-64%2Fstable%2Fdebian+package
 4. minikube config set driver virtualbox
 5. minikube start
+6. minikube status
