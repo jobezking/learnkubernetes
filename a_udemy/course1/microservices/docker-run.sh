79b8f6cdf084f9768506a4abfa49980b2fb4e5f5
Job King <jobezking@gmail.com>
2026-08-22 12:32:03 -0400

    push

--- a_udemy/course1/microservices/docker-run.sh
+++ a_udemy/course1/microservices/docker-run.sh
@@ -13,4 +13,14 @@ docker run -d --name=worker worker --link db:db --link redis:redis     # .NET ap
 Steps
 1. Deploy pods (or replicasets or deployments)
 2. Enable connectivity between services. Need to know which applications are needed by what services
-3. 
+3a. Create redis service named redis so it can be accessed by voting and worker apps (ClusterIP)
+3b. Create db service named db so it can be accessed by result and worker apps (ClusterIP). The db pod will need username and password
+3c. External access services for voting and result apps (Nodeport)
+5 total ports. 4 services: one for redis, one for Postgres (internal, ClusterIP) plus external for result and voting (Nodeport). No worker service
+
+Containers:
+kodekloud/examplevotingapp_vote:v1
+kodekloud/examplevotingapp_result:v1
+kodekloud/examplevotingapp_worker:v1
+redis
+postgresql
