e167b535ceb3d0ae125437ceda3fc5bfc7b37b16
Job King <jobezking@gmail.com>
2026-08-22 11:56:40 -0400

    push

--- a_udemy/course1/microservices/docker-run.sh
+++ a_udemy/course1/microservices/docker-run.sh
@@ -1,5 +1,5 @@
 docker run -d --name=redis redis                  # in memory data store db
 docker run -d --name=db postgres:9.4              # backend db
-docker run -d --name=vote -p 5000:80 voting-app    # Python voting app
-docker run -d --name=result -p 5001:80 result-app  # node.js app to build webpage to show results
-docker run -d --name=worker worker                 # .NET app background service
+docker run -d --name=vote -p 5000:80 voting-app --link redis:redis   # Python voting app
+docker run -d --name=result -p 5001:80 result-app --link db:db # node.js app to build webpage to show results
+docker run -d --name=worker worker --link db:db --link redis:redis     # .NET app background service
