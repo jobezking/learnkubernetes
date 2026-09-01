aa989b2da86e7103bc384af228d30faa8ed00a8b
Job King <jobezking@gmail.com>
2026-09-01 18:07:53 -0400

    push

--- a_udemy/course2/notes.sh
+++ a_udemy/course2/notes.sh
@@ -5,3 +5,4 @@ kubectl scale rs myapp-replicaset --replicas=5
 kubectl scale --replicas=5 -f replicaset-definition.yml
 kubectl delete rs myapp-replicaset
 kubectl replace -f replicaset-definition.yml
+#Deployments automatically create ReplicaSets with the name of the deployment and create pods including the name of the deployment
