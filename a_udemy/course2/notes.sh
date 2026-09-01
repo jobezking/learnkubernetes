c729f82114e9eef8e15463ffd3a5b8af2b8589ac
Job King <jobezking@gmail.com>
2026-09-01 10:31:15 -0400

    push

--- a_udemy/course2/notes.sh
+++ a_udemy/course2/notes.sh
@@ -1,2 +1,5 @@
 kubectl get pods -n kube-system  # shows kubernetes system pods
-kubectl run nginx --image=nginx 
+kubectl run nginx --image=nginx
+kubectl scale replicaset myapp-replicaset --replicas=5
+kubectl scale rs myapp-replicaset --replicas=5
+kubectl scale --replicas=5 -f replicaset-definition.yml
