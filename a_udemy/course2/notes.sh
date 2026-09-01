76d56af920592b4d2d68cb596070934e4f30668c
Job King <jobezking@gmail.com>
2026-09-01 10:33:41 -0400

    update notes.sh with replicaset

--- a_udemy/course2/notes.sh
+++ a_udemy/course2/notes.sh
@@ -3,3 +3,5 @@ kubectl run nginx --image=nginx
 kubectl scale replicaset myapp-replicaset --replicas=5
 kubectl scale rs myapp-replicaset --replicas=5
 kubectl scale --replicas=5 -f replicaset-definition.yml
+kubectl delete rs myapp-replicaset
+kubectl replace -f replicaset-definition.yml
