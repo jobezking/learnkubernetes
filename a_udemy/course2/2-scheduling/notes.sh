0b13b82374b49527e401b212faf5f39f14360f4d
Job King <jobezking@gmail.com>
2026-09-02 17:44:17 -0400

    update

--- a_udemy/course2/2-scheduling/notes.sh
+++ a_udemy/course2/2-scheduling/notes.sh
@@ -4,3 +4,4 @@ kubectl describe pod nginx | grep -i node  # check the node the pod is running o
 kubectl get pods -n kube-system   # see if scheduler pod is running
 kubectl replace --force -f pod-definition.yaml  # force replace a pod definition (deletes the pod and creates a new one)
 kubectl get pods --selector app=App1 # get pods with label app=App1
+kubectl get pods --selector app=App1 --no-headers | wc -l  # count the number of pods with label app=App1
