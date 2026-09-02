68353ecc65e21fb2b36ddf0fb1ad7aba6761d6d5
Job King <jobezking@gmail.com>
2026-09-02 10:30:01 -0400

    update notes.sh

--- a_udemy/course2/2-scheduling/notes.sh
+++ a_udemy/course2/2-scheduling/notes.sh
@@ -1,2 +1,4 @@
 kubectl get componentstatuses
-kubectl get cs
+kubectl get cs   # see component status
+kubectl describe pod nginx | grep -i node  # check the node the pod is running on
+kubectl get pods -n kube-system   # see if scheduler pod is running
