b27f981edacb3fae3951cf2ca2995934572b64ee
Job King <jobezking@gmail.com>
2026-09-04 18:01:29 -0400

    static pod

--- a_udemy/course2/2-scheduling/notes.sh
+++ a_udemy/course2/2-scheduling/notes.sh
@@ -39,3 +39,6 @@ Exists  # node must have the label, but the value does not matter
 
 # DaemonSets runs one copy of the pod on each node in the cluster. When a new node is added to the cluster, a copy of the pod is 
 # automatically scheduled on that node. When a node is removed the pod is removed. Ensures that a copy of the pod is running on all nodes in the cluster.
+
+#Create static pod:
+kubectl run --restart=Never --image=busybox static-busybox --dry-run=client -o yaml --command -- sleep 1000 > /etc/kubernetes/manifests/static-busybox.yaml
