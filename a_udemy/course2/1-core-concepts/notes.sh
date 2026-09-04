8e8334379f371354273f1bb68b5c66d709f8c183
Job King <jobezking@gmail.com>
2026-09-04 10:36:35 -0400

    meap

--- a_udemy/course2/1-core-concepts/notes.sh
+++ a_udemy/course2/1-core-concepts/notes.sh
@@ -2,7 +2,9 @@ kubectl get pods  # shows all pods in the default namespace
 kubectl get pods --namespace=kube-system  # shows all pods in the kube-system namespace
 kubectl get pods -n kube-system  # shows kubernetes system pods
 kubectl get pods --all-namespaces  # shows all pods in all namespaces
-kubectl get deployment blue -o yaml > blueconfig.yaml
+kubectl run nginx --image=nginx # create a pod named nginx with the nginx image
+kubectl get pod nginx -o yaml > nginx-config.yaml # saves the pod config to a yaml file
+kubectl get deployment blue -o yaml > blueconfig.yaml  # saves the deployment config to a yaml file
 kubectl create -f pod-definition.yml --namespace=dev # create a pod from a definition file in namespace dev
 kubectl run nginx --image=nginx   # create a pod named nginx with the nginx image
 kubectl run nginx --image=nginx --namespace=dev   # create a pod named nginx with the nginx image in namespace dev
