86ba613416750e5d38103ee798691cbc5dea289a
Job King <jobezking@gmail.com>
2026-09-04 20:01:45 -0400

    update

--- a_udemy/course2/2-scheduling/notes.sh
+++ a_udemy/course2/2-scheduling/notes.sh
@@ -1,5 +1,6 @@
 kubectl get componentstatuses
 kubectl get priorityclasses # see all priority classes
+kubectl get pods -o custom-columns="NAME:.metadata.name,PRIORITY:.spec.priorityClassName"
 kubectl get cs   # see component status
 kubectl describe pod nginx | grep -i node  # check the node the pod is running on
 kubectl get pods -n kube-system   # see if scheduler pod is running
