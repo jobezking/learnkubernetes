c4993c7ef799fbab2d6235cd706cb3732db53016
Job King <jobezking@gmail.com>
2026-09-06 17:08:22 -0400

    update

--- a_udemy/course2/5-app-lifecycle-mgmt/notes.sh
+++ a_udemy/course2/5-app-lifecycle-mgmt/notes.sh
@@ -16,7 +16,9 @@ kubectl set image deployment/myapp-deployment nginx-container=nginx:latest  # up
 kubectl create -f deployment-definition.yml  # creates original deployment
 kubectl get deployments # Lists all deployments
 kubectl apply -f deployment-definition-mod.yml  # creates modified deployment, triggers rolling update
-kubectl set image deployment/myapp-deployment nginx-container=nginx:latest  # same as above but imperative
+kubectl set image deployment/<deployment-name> <container>=<image> # same as above but imperative
+kubectl set image deployment/myapp-deployment nginx-container=nginx:latest  # example
+kubectl set image deployment/frontend simple-webapp=kodekloud/webapp-color:v2 # example
 kubectl rollout status deployment/myapp-deployment  # Check the status of a deployment rollout
 kubectl rollout history deployment/myapp-deployment  # Check the rollout history of a deployment
 kubectl rollout undo deployment/myapp-deployment  # Rollback to the previous deployment
