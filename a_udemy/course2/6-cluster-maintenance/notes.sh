78b1d9a72b8d1101f19848a7086bd1568c7b2e01
Job King <jobezking@gmail.com>
2026-09-09 13:54:29 -0400

    update

--- a_udemy/course2/6-cluster-maintenance/notes.sh
+++ a_udemy/course2/6-cluster-maintenance/notes.sh
@@ -1,3 +1,5 @@
+kubectl get nodes # check node status
+cat /etc/*release # check OS version
 kubectl drain <node-name> --ignore-daemonsets --delete-local-data # do before taking down node for maintenance
 # pods are recreated on another node and node is marked as unschedulable
 kubectl uncordon <node-name> # do after maintenance is complete to allow scheduling of pods on the node again
@@ -7,3 +9,21 @@ kubectl get pods -o wide # check where pods are running
 # Upgrade k8s one minor version at a time, e.g. 1.25.x -> 1.26.x -> 1.27.x
 # First upgrade master node then upgrade worker nodes
 kubeadm upgrade plan # check for available upgrades
+
+#Upgrade master node
+sudo apt-get upgrade -y kubeadm=<version> # upgrade kubeadm to the specified version
+kubeadm upgrade apply <version> # upgrade master node to the specified version
+
+sudo apt-get upgrade -y kubelet=<version>
+sudo systemctl daemon-reload
+sudo systemctl restart kubelet
+kubectl get nodes # check node status
+
+#Upgrade worker nodes
+kubectl drain <node-name> --ignore-daemonsets --delete-local-data # drain the node before upgrade
+sudo apt-get upgrade -y kubeadm=<version>
+sudo apt-get upgrade -y kubelet=<version>
+kubeadm upgrade node config --kubelet-version <version> # upgrade kubelet on the node
+sudo systemctl daemon-reload
+sudo systemctl restart kubelet
+kubectl uncordon <node-name> # uncordon the node after upgrade
