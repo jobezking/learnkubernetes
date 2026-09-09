fbef3a896cb400dc192537201f481442e2d30f81
Job King <jobezking@gmail.com>
2026-09-09 11:59:43 -0400

    samba

--- a_udemy/course2/6-cluster-maintenance/notes.sh
+++ a_udemy/course2/6-cluster-maintenance/notes.sh
@@ -2,3 +2,4 @@ kubectl drain <node-name> --ignore-daemonsets --delete-local-data # do before ta
 # pods are recreated on another node and node is marked as unschedulable
 kubectl uncordon <node-name> # do after maintenance is complete to allow scheduling of pods on the node again
 kubectl cordon <node-name> # mark node as unschedulable to prevent scheduling of pods on the node
+kubectl get pods -o wide # check where pods are running
