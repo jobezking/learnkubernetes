35395420ed91e626f1a2a95a09bcad2a9d5e4c72
Job King <jobezking@gmail.com>
2026-09-10 17:28:37 -0400

    update

--- a_udemy/course2/7-security/notes.sh
+++ a_udemy/course2/7-security/notes.sh
@@ -1,4 +1,23 @@
 ## Certificate Authority (CA)
+# Server certificates
 openssl genrsa -out ca.key 2048                                     # generate a private key for the CA
 openssl req -new -key ca.key -subj "/CN=KUBERNETES-CA" -out ca.csr  # certificate signing request
 openssl x509 -req -in ca.csr -signkey ca.key -out ca.crt            # self-sign the CA certificate
+# Client certificates
+# admin user
+openssl genrsa -out admin.key 2048                                 # generate a private key for the client
+openssl req -new -key admin.key -subj "/CN=kube-admin" -out admin.csr  # certificate signing request
+openssl x509 -req -in admin.csr -CA ca.crt -CAkey ca.key -out admin.crt  # sign the client certificate with the CA using server certificate key
+# repeat for all other components that access the kube-api-server (kubelet, kube-proxy, kube-controller-manager, kube-scheduler, etcd, etc.)
+#kube-scheduler
+openssl genrsa -out scheduler.key 2048                                 # generate a private key for the client
+openssl req -new -key scheduler.key -subj "/CN=system:kube-scheduler" -out scheduler.csr  # certificate signing request
+openssl x509 -req -in scheduler.csr -CA ca.crt -CAkey ca.key -out scheduler.crt  # sign the client certificate with the CA using server certificate key
+#kube-controller-manager
+openssl genrsa -out controller-manager.key 2048                                 # generate a private key for the client
+openssl req -new -key controller-manager.key -subj "/CN=system:kube-controller-manager" -out controller-manager.csr  # certificate signing request
+openssl x509 -req -in controller-manager.csr -CA ca.crt -CAkey ca.key -out controller-manager.crt  # sign the client certificate with the CA using server certificate key
+#kube-proxy
+openssl genrsa -out kube-proxy.key 2048                                 # generate a private key for the client
+openssl req -new -key kube-proxy.key -subj "/CN=system:kube-proxy" -out kube-proxy.csr  # certificate signing request
+openssl x509 -req -in kube-proxy.csr -CA ca.crt -CAkey ca.key -out kube-proxy.crt  # sign the client certificate with the CA using server certificate key
