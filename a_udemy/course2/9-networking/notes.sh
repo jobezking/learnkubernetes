e912d9e4d425bcff09be25aa6e29eb6a9a11261f
Job King <jobezking@gmail.com>
2026-09-12 19:28:07 -0400

    update notes.sh

--- a_udemy/course2/9-networking/notes.sh
+++ a_udemy/course2/9-networking/notes.sh
@@ -1,5 +1,7 @@
 ip link  #brief view of network interfaces
-ip addr add 192.168.1.10/24 dev eth0
-route  # displays the kernel's routing table
+ip addr  # see ip addresses asssigned to interfaces
+ip addr add 192.168.1.10/24 dev eth0  # used to set IP addresses on interfaces
+route (or ip route) # displays the kernel's routing table
 ip route add 192.168.2.0/24 via 192.168.1.1  # route from one network to another via a router whose ip is 192.168.1.1
 ip route add default via 192.168.2.1  # default gateway
+cat /proc/sys/net/ipv4/ip_forward
