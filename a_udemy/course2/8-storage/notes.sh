7b4c603594d874f9353935613a0659e2a693a26e
Job King <jobezking@gmail.com>
2026-09-12 15:40:46 -0400

    update

--- a_udemy/course2/8-storage/notes.sh
+++ a_udemy/course2/8-storage/notes.sh
@@ -4,3 +4,7 @@ docker volume create data_volume
 docker run -v data_volume:/var/lib/mysql mysql
 docker run -v data_volume2:/var/lib/mysql mysql
 docker run --moount type=bind, source=data/mysql, target=/var/lib/mysql mysql
+docker run -it --name mysql --mount src=ebs-vol,target=/var/lib/mysql mysql  # uses the AWS EBS driver to save container data to cloud
+
+# Kubernetes volumes
+# need to attach volumes to pods for data processed by pod to be preserved when pod is destroyed
