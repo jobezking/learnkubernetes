79112a88aa18f3525a240dded7ce91179f9f3ebb
Job King <jobezking@gmail.com>
2026-09-12 19:24:29 -0400

    update

--- a_udemy/course2/9-networking/notes.sh
+++ a_udemy/course2/9-networking/notes.sh
@@ -1,1 +1,5 @@
 ip link  #brief view of network interfaces
+ip addr add 192.168.1.10/24 dev eth0
+route  # displays the kernel's routing table
+ip route add 192.168.2.0/24 via 192.168.1.1  # route from one network to another via a router whose ip is 192.168.1.1
+ip route add default via 192.168.2.1  # default gateway
