dce0e803ac07042767c6c91819748ddc9e653a31
Job King <jobezking@gmail.com>
2026-09-12 19:49:18 -0400

    update

--- a_udemy/course2/9-networking/notes.sh
+++ a_udemy/course2/9-networking/notes.sh
@@ -1,3 +1,4 @@
+# Basic Networking
 ip link  #brief view of network interfaces
 ip addr  # see ip addresses asssigned to interfaces
 ip addr add 192.168.1.10/24 dev eth0  # used to set IP addresses on interfaces
@@ -5,3 +6,11 @@ route (or ip route) # displays the kernel's routing table
 ip route add 192.168.2.0/24 via 192.168.1.1  # route from one network to another via a router whose ip is 192.168.1.1
 ip route add default via 192.168.2.1  # default gateway
 cat /proc/sys/net/ipv4/ip_forward
+# Basic DNS
+/etc/resolv.conf  # contains address of DNS server
+A: IP to hostname
+AAAA: hostname to IPv6
+CNAME: mapping one DNS name to another
+ping
+nslookup
+dig
