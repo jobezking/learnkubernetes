771afa3c848d4f5c6ce36a01a4958e5d0612ea70
Job King <jobezking@gmail.com>
2026-09-14 16:30:10 -0400

    update

--- a_udemy/course2/11-installation-with-kubeadmn/notes.sh
+++ a_udemy/course2/11-installation-with-kubeadmn/notes.sh
@@ -10,3 +10,12 @@ The vagrant file used in the next video is available here:
 https://github.com/kodekloudhub/certified-kubernetes-administrator-course
 Here's the link to the documentation:
 https://kubernetes.io/docs/setup/production-environment/tools/kubeadm/install-kubeadm/
+
+# On all 3 nodes
+sudo apt-get update; sudo apt upgrade -y; sudo apt-get install -y apt-transport-https ca-certificates curl gpg
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.37/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.37/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
+sudo apt-get update; sudo apt-get install -y kubelet kubeadm kubectl; sudo apt-mark hold kubelet kubeadm kubectl
+sudo apt install -y containerd
+sudo mkdir -p /etc/containerd
+containerd config default | sed 's/SystemdCgroup = false/SystemdCgroup = true/' | sudo tee /etc/containerd/config.toml
