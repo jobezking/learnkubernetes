293aabaf428b70f8a37885cf0d0f32425bcc7b8a
Job King <jobezking@gmail.com>
2026-09-13 21:42:31 -0400

    update

--- a_udemy/course2/9-networking/notes.sh
+++ a_udemy/course2/9-networking/notes.sh
@@ -41,3 +41,19 @@ cat /var/lib/kubelet/config.yaml
 
 kubectl get ingress
 kubectl describe ingress ingress-wear-watch
+
+in k8s version 1.20+ we can create an Ingress resource from the imperative way like this:-
+
+Format - kubectl create ingress <ingress-name> --rule="host/path=service:port"
+
+Example - kubectl create ingress ingress-test --rule="wear.my-online-store.com/wear*=wear-service:80"
+
+Find more information and examples in the below reference link:-
+
+https://kubernetes.io/docs/reference/generated/kubectl/kubectl-commands#-em-ingress-em-
+
+References:-
+
+https://kubernetes.io/docs/concepts/services-networking/ingress
+
+https://kubernetes.io/docs/concepts/services-networking/ingress/#path-types
