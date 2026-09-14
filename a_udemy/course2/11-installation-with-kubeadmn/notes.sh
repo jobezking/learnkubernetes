ed1a39da10148a4c4629f99a90eeea352df322eb
Job King <jobezking@gmail.com>
2026-09-14 13:14:56 -0400

    update

--- a_udemy/course2/11-installation-with-kubeadmn/notes.sh
+++ a_udemy/course2/11-installation-with-kubeadmn/notes.sh
@@ -2,3 +2,6 @@
 1. Create 3 VMs: 1 master and 2 workers. In a real cluster you need 3 masters because of the 2/N + 1 quorum requirement for etcd server.
 2. Install containerd on all 3 nodes. 
 3. Install kubeadm on all 3 nodes.
+4. Initialize the master server.
+5. Install pod network on all 3 servers
+6. Join the worker nodes to the master.
