f1b170f9e791da6b14e582798c5de27293eb1380
Job King <jobezking@gmail.com>
2026-09-14 20:54:26 -0400

    update

--- a_udemy/course2/12-helm/notes.sh
+++ a_udemy/course2/12-helm/notes.sh
@@ -34,11 +34,11 @@ helm repo --help
 # Install app with helm
 1. search for app in repositories. The first is preferred. Or use artifacthub.io
 helm search hub wordpress  # all repositories are listed
-helm search repo wordpress
 
 # Note this among the results: https://artifacthub.io/packages/helm/bitnami/wo...
 
 helm repo add bitnami https://charts.bitnami.com/bitnami
+helm search repo wordpress  # searches only installed repo(s)
 help install my-release bitnami/wordpress
 # To customize:
 helm install --set wordpressBlogName="Helm Tutorials" --set wordpressEmail="john@example.com" my-release bitnami/wordpress
