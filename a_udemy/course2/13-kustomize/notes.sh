c4811228915c08974aae6f2de2a8a0c4903143bf
Job King <jobezking@gmail.com>
2026-09-15 18:55:39 -0400

    update

--- a_udemy/course2/13-kustomize/notes.sh
+++ a_udemy/course2/13-kustomize/notes.sh
@@ -1,3 +1,6 @@
 Installation Prerequisites
 1. running Kubernetes cluster
 2. kubectl installed locally and connected to your cluster (.kube/conf)
+curl -s "https://raw.githubusercontent.com/kubernetes-sigs/kustomize/master/hack/install_kustomize.sh" | bash
+sudo mv kustomize /usr/local/bin/
+kustomize version --short
