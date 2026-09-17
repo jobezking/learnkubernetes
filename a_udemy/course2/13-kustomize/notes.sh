ca8f6acb5bd6e0fc7134256842409aa1a075d85b
Job King <jobezking@gmail.com>
2026-09-17 00:52:59 -0400

    update

--- a_udemy/course2/13-kustomize/notes.sh
+++ a_udemy/course2/13-kustomize/notes.sh
@@ -1,3 +1,5 @@
+FIND AN ACTUAL GOOD SOURCE TO STUDY KUSTOMIZATION
+
 # Installation Prerequisites
 # 1. running Kubernetes cluster
 # 2. kubectl installed locally and connected to your cluster (.kube/conf)
