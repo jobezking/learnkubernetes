10a2d58ef40de33b8b170dc5af335db254262730
Job King <jobezking@gmail.com>
2026-09-17 16:24:07 -0400

    update

--- a_udemy/course2/14-troubleshooting/notes.sh
+++ a_udemy/course2/14-troubleshooting/notes.sh
@@ -6,3 +6,22 @@ describe: events for pod or deployment
 logs: logs for pod or deployment
 kubectl logs web -f
 kubectl logs web -f --previous
+https://kubernetes.io/docs/tasks/debug/debug-application
+
+# Control plane troubleshooting
+kubectl get nodes
+kubectl get pods
+kubectl get pods -n kube-system  # if deployed as pods using kubeadm
+# if deployed as services
+# master node
+service kube-apiserver status   
+service kube-controller-manager status 
+service kube-scheduler status
+# worker node
+service kubelet status
+service kube-proxy status
+# Check control plane component logs
+kubectl logs kube-apiserver-master -n kube-system  # if kubeadm deployed pod
+sudo journalctl -u kube-apiserver # if deployed as service
+https://kubernetes.io/docs/tasks/debug/debug-cluster
+
