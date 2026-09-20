50f8bea5cb7cf3eaa9359bde4ceee7812c004b4d
Job King <jobezking@gmail.com>
2026-09-20 15:02:37 -0400

    update

--- a_udemy/course2/15-json-path/notes.sh
+++ a_udemy/course2/15-json-path/notes.sh
@@ -23,3 +23,8 @@ kubectl get nodes --sort-by=.status.capacity.cpu
 
 kubectl get nodes -o json > nodes.json
 '{.items[*].metadata.name}'
+kubectl get nodes -o jsonpath='{.items[*].status.nodeInfo.osImage}' > /opt/outputs/nodes_os.txt
+kubectl get pv --sort-by=.spec.capacity.storage > /opt/outputs/storage-capacity-sorted.txt
+
+kubectl get pv --sort-by=.spec.capacity.storage -o=custom-columns=NAME:.metadata.name,CAPACITY:.spec.capacity.storage > /opt/outputs/pv-and-capacity-sorted.txt
+kubectl config view --kubeconfig=my-kube-config -o jsonpath="{.contexts[?(@.context.user=='aws-user')].name}" > /opt/outputs/aws-context-name
